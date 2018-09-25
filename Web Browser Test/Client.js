0aeb9d9cec4d3d66220e67775b81b067f453ce46
Garrett Wells <sprintepic1122@gmail.com>
2018-09-25 16:47:01 -0400

    get the client properly running

--- Web Browser Test/Client.js
+++ Web Browser Test/Client.js
@@ -4,9 +4,26 @@ const server = '127.0.0.1';
 const port = 3000;
 
 //create the client
-const request = http.request({
+
+const options = {
     port: port,
     host: server,
-    method: 'PUT',
+    headers: {
+        'Content-Type': 'text/plain'
+    },
+    method: 'GET',
     path: '/test'
+};
+
+const request = http.request(options, (res) => {
+    res.read();
+    res.on('data', (chunk) => {
+        console.log(`the server's response is ${chunk}\n`)
+    });
+    res.on('end', () => {
+        console.log('end of the response was reached\n')
+    })
 });
+
+
+request.end()
--- Web Browser Test/Server.js
+++ Web Browser Test/Server.js
@@ -7,9 +7,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 
     //Set the response HTTP header with HTTP status and Content type
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.writeHead(200, {'Content-Type': 'text/plain',
+                        'Header2': 'value2'});
+    res.write('Hello World\n')
+    res.end(console.log('received request'));
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
